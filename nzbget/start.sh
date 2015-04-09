344052732a1e4c7c2de061c8b343f46447716557
Ivan Reyes <ivanreyesconde@gmail.com>
2015-04-09 13:35:40 -0500

    Linked containers

--- nzbget/start.sh
+++ nzbget/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-[ ! -f /data/nzbget.conf ] && cp /usr/share/nzbget/webui/nzbget.conf /config/nzbget.conf
+#[ ! -f /data/nzbget.conf ] && cp /usr/share/nzbget/webui/nzbget.conf /config/nzbget.conf
 
 nzbget --daemon --configfile /config/nzbget.conf
