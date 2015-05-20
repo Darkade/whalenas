3f15e65c9c570cdc6c1f9f95e1ff6db2c943b7f7
Ivan Reyes <ivanreyesconde@gmail.com>
2015-05-20 13:45:52 -0500

    NZBget working to latest version (15.0)

--- nzbget/start.sh
+++ nzbget/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-#[ ! -f /data/nzbget.conf ] && cp /usr/share/nzbget/webui/nzbget.conf /config/nzbget.conf
-
-nzbget --daemon --configfile /config/nzbget.conf
+/opt/nzbget/nzbget -s -o OutputMode=color
+# --configfile /config/nzbget.conf
